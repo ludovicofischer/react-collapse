370d272eeacecf6e5710da491e00a98ff8f50672
Ludovico Fischer <livrerie@gmail.com>
2019-01-04 10:56:49 +0100

    chore: Add CommonJS module format.

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,15 @@ import babel from 'rollup-plugin-babel';
 
 module.exports = {
   input: 'src/index.js',
-  output: {
+  output: [{
     file: 'dist/index.js',
     format: 'esm'
   },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    }
+  ],
  plugins: [
   resolve({browser: true, jsnext: true, extensions: ['.mjs', '.js', '.json', '.node', '.ts', '.tsx']}),
   commonjs({include: 'node_modules/**'}),
